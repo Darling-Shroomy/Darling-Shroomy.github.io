7333352ca297f129143df347fcd9d3d7c6065186
Darling-Shroomy <samantha.martin@stu.cvccworks.edu>
2024-10-24 13:03:00 -0400

    Add files via upload

---
+++ apply10/scripts/script.js
@@ -0,0 +1,6 @@
+function content(){
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#43435c";
+    text.style.fontSize = "2em";
+}
